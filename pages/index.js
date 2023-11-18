13f4749d29443c8cfa43a18a81fc88216ee9ee5c
Adrien <77799977+spieredd@users.noreply.github.com>
2023-11-18 16:40:58 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import { useFirebase } from "../context/firebaseContext";
 import PayPalButton from "../components/PayPalButton";
 import Lightbox from "react-image-lightbox";
 import "react-image-lightbox/style.css";
+import { doc, deleteDoc } from "firebase/firestore";
 
 import { Fragment, useState, useEffect } from "react";
 import { Dialog, Menu, Transition, Listbox } from "@headlessui/react";
@@ -53,6 +54,7 @@ function classNames(...classes) {
 }
 
 export default function Home() {
+  const { firestore } = useFirebase();
   const [photoIndex, setPhotoIndex] = useState(null);
   const [isOpen, setIsOpen] = useState(false);
   const [currentImage, setCurrentImage] = useState("");
@@ -106,6 +108,19 @@ export default function Home() {
       : originalOffers;
 
     setOffers(filteredData);
+  };
+
+  const deleteOffer = async (offerId) => {
+    console.log(offerId);
+    try {
+      // Assuming offers have unique IDs stored as their document key
+      const offerRef = doc(firestore, "offers", offerId);
+      await deleteDoc(offerRef);
+      // Remove the offer from the local state
+      setOffers((offers) => offers.filter((offer) => offer.id !== offerId));
+    } catch (error) {
+      console.error("Error deleting offer:", error);
+    }
   };
 
   return (
@@ -521,11 +536,29 @@ export default function Home() {
                           <div className="inline-flex items-center rounded-md bg-yellow-400/10 px-2 py-1 text-xs font-medium text-yellow-500 ring-1 ring-inset ring-yellow-400/20">
                             Price: {offer.price} €
                           </div>
-                          {/* {user &&
-                            <div className='mt-1'>
-                              <PayPalButton total={offer.price} className="z-0" />
-                            </div>
-                          } */}
+                          {user && user.email === offer.email && (
+                            <button
+                              type="button"
+                              onClick={() => deleteOffer(offer.id)}
+                              className="rounded flex items-center gap-2 bg-red-700 px-2 py-1 text-sm font-semibold text-white shadow-sm hover:bg-red-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+                            >
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth={1.5}
+                                stroke="currentColor"
+                                className="w-4 h-4"
+                              >
+                                <path
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                  d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                                />
+                              </svg> Delete
+                              
+                            </button>
+                          )}
                         </div>
                       </div>
                       <div className="flex justify-end items-center w-[200px] p-2 rounded">
